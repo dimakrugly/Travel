25cdd8fb09f2e95a649bceecfbc61055def355a7
dimakrugly <106512918+dimakrugly@users.noreply.github.com>
2022-07-02 16:37:35 +0300

    add: added slider, fixed element issues

---
+++ js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+        $('.owl-carousel').owlCarousel({
+        loop:false,
+        margin:60,
+        center: true,
+        // dotsData: true,
+        startPosition: 1,
+        responsive:{
+            0:{
+                items:1.75
+            },
+            // 600:{
+            //     items:2
+            // },
+           // 1000:{
+           //     items:3
+           // }
+        }
+    })
+  });
+  
